cfc48f42b9d7f5bb72cfb2f26f3bb65d24f98421
005Dikshant <005Dikshant@gmail.com>
2021-01-29 23:49:29 +0530

    Multiple Activities

---
+++ MultipleActivityDemo/app/src/main/java/dikshant/myapp/multipleactivitydemo/MainActivity.java
@@ -0,0 +1,22 @@
+package dikshant.myapp.multipleactivitydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void goToNext(View view){
+        Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+        intent.putExtra("age",22);
+
+        startActivity(intent);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
